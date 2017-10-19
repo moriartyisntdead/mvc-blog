6711c573625b1f28e38f366b1fd119c37d83da79
GV_programist <inifrey@gmail.com>
2017-10-19 10:52:33 +0300

    Исправление ошибки

--- models/model_articles.php
+++ models/model_articles.php
@@ -6,8 +6,8 @@ Class Model_Articles Extends Models_Base{
     }
 
     public function getCategoryAnchor(){
-        $stmt = $this->db->prepare("select anchor from category order by id desc  LIMIT 1");
-        $stmt->execute(array());
+        $stmt = $this->db->prepare("select anchor from categories WHERE id=? order by id desc  LIMIT 1");
+        $stmt->execute(array($this->category_id));
         if ($stmt->rowCount() == 0) return false;
         else return $stmt->fetchObject()->anchor;
     }
